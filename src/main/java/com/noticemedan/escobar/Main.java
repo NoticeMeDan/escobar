c2cffa48418c374dfdc5ffbde9431f41a2346e09
Vilfred <Vilfredsikker@gmail.com>
2018-10-10 18:39:58 +0200

    Initial state of methods

--- src/main/java/com/noticemedan/escobar/Main.java
+++ src/main/java/com/noticemedan/escobar/Main.java
@@ -1,19 +1,46 @@
 package com.noticemedan.escobar;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class Main {
 	public static void main(String[] args) {
-		String arg = args[0];
-		if(arg.equals("broker")){
+		String object = args[0];
+		int port = Integer.parseInt(args[1]);
+		String group = args[2];
 
+		if(object.equals("broker")){
+			createBroker(port, group);
 		}
-		else if(arg.equals("source")){
-
+		else if(object.equals("source")){
+			createSource();
 		}
-		else if(arg.equals("sink")){
-
+		else if(object.equals("sink")){
+			createSink();
 		}
-		else{
+		else {
 			System.out.println("Please provide broker, source or sink as argument");
 		}
+	}
+
+	private static void createSink() {
+	}
+
+	private static void createSource() {
+	}
+
+	private static void createBroker(int port, String groupName) {
+		InetAddress group;
+		MulticastSocket broker;
+		try {
+			group = InetAddress.getByName(groupName);
+			broker = new MulticastSocket(port);
+			broker.joinGroup(group);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 }

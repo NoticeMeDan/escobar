795997613f73fb87cc68bd0333830b6b3535a052
alol <alol@itu.dk>
2018-10-10 20:10:42 +0200

    Created all 3 methods and a test for broker

--- src/main/java/com/noticemedan/escobar/Main.java
+++ src/main/java/com/noticemedan/escobar/Main.java
@@ -1,33 +1,70 @@
 package com.noticemedan.escobar;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		String object = args[0];
 		int port = Integer.parseInt(args[1]);
 		String group = args[2];
-
 		if(object.equals("broker")){
 			createBroker(port, group);
 		}
 		else if(object.equals("source")){
-			createSource();
+			createSource(port, group);
 		}
 		else if(object.equals("sink")){
-			createSink();
+			createSink(port, group);
 		}
 		else {
 			System.out.println("Please provide broker, source or sink as argument");
 		}
 	}
 
-	private static void createSink() {
+	private static void createSink(int port, String groupName) {
+		InetAddress group;
+		MulticastSocket sink;
+		try {
+			group = InetAddress.getByName(groupName);
+			sink = new MulticastSocket(port);
+			sink.joinGroup(group);
+			byte[] buf = new byte[1000];
+
+			while(true) {
+				DatagramPacket dp = new DatagramPacket(buf, buf.length);
+				sink.receive(dp);
+				byte[] data = dp.getData();
+				String msg = new String(data,0, data.length);
+				System.out.println(msg);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private static void createSource() {
+	private static void createSource(int port, String groupName) {
+		InetAddress group;
+		MulticastSocket source;
+		try {
+			group = InetAddress.getByName(groupName);
+			source = new MulticastSocket(port);
+			source.joinGroup(group);
+
+			Scanner sc = new Scanner(System.in);
+			byte[] buf = new byte[1000];
+			while(true){
+				byte[] msg = sc.nextLine().getBytes();
+				source.send(new DatagramPacket(msg, msg.length, group, port));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	private static void createBroker(int port, String groupName) {
@@ -37,10 +74,9 @@ public class Main {
 			group = InetAddress.getByName(groupName);
 			broker = new MulticastSocket(port);
 			broker.joinGroup(group);
+			while(true);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-
 	}
 }
--- src/test/java/com/noticemedan/escobar/MainTest.java
+++ src/test/java/com/noticemedan/escobar/MainTest.java
@@ -1,6 +1,13 @@
 package com.noticemedan.escobar;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 
 import static org.testng.Assert.*;
 
@@ -11,11 +18,33 @@ public class MainTest {
     }
 
     @Test
-    public void testCreateBroker() {
+    public void test_main_given_broker_succesfully_creates_broker() throws Exception {
+        //Try to create the broker
+        String[] args = {"broker", "7007", "230.0.0.0"};
+        Main.main(args);
+
+        //Setup the socket
+        InetAddress group = InetAddress.getByName("230.0.0.0");
+        MulticastSocket s = new MulticastSocket(7007);
+
+        //Try to join
+        s.joinGroup(group);
+
+        //If joinGroup doesn't throw an IOException, it works.
+        Assert.assertTrue(true);
     }
 
+
     @Test
-    public void testCreateSink() {
+    public void testCreateSink() throws Exception{
+        //Create a sink object
+        String[] args = {"sink", "7007", "230.0.0.0"};
+        Main.main(args);
+
+        InetAddress group = InetAddress.getByName("230.0.0.0");
+
+        byte[] buffer = new byte[1000];
+        DatagramPacket dp = new DatagramPacket(buffer, 1000, group, 7007);
     }
 
     @Test

a2ca7dd285bc10d6377f73aaf5c9708fa9e31c3f
alol <alol@itu.dk>
2018-10-10 18:20:42 +0200

    Quick template created

--- src/main/java/com/noticemedan/escobar/Main.java
+++ src/main/java/com/noticemedan/escobar/Main.java
@@ -2,6 +2,18 @@ package com.noticemedan.escobar;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hi mom");
+		String arg = args[0];
+		if(arg.equals("broker")){
+
+		}
+		else if(arg.equals("source")){
+
+		}
+		else if(arg.equals("sink")){
+
+		}
+		else{
+			System.out.println("Please provide broker, source or sink as argument");
+		}
 	}
 }
